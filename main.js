f13fd5ed7d06d55c85014938546d1455cc2e44b8
gitHubMIH <114809703+gitHubMIH@users.noreply.github.com>
2022-12-21 14:45:44 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+video = "";
+status="";
+function preload() {
+    video = createVideo("video.mp4");
+}
+function setup() {
+    canvas=createCanvas(480, 380);
+    canvas.center();
+    video.hide();
+}
+function draw() {
+    image(video, 0, 0, 480, 380);
+}
+function start() {
+objectDetector=ml5.objectDetector("cocossd", modelLoaded);
+document.getElementById("status").innerHTML="Status : detectando objetos";
+}
+function modelLoaded() {
+    console.log("O modelo foi carregado");
+status=true;
+video.loop();
+video.speed(1);
+video.volume(0);
+}
